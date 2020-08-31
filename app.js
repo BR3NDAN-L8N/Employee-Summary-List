e00cb895084006a905dc8ecc6943548dfe06d408
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-08-31 08:04:33 -0400

    finalized code

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const Employee = require("./lib/Employee");
 
 const employeeArr = [];
 const currentEmployee = {
